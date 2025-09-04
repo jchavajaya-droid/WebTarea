8b8b5b035b20641cee201ba300ceb4481260b4d6
juana <jchavajaya@miumg.edu.gt>
2025-09-04 00:23:30 -0600

    Actualización de tarea y nuevos archivos

--- WebTarea/settings.py
+++ WebTarea/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guatemala'
 
 USE_I18N = True
 
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+# Si tu carpeta static no está en la app, indica la ruta completa
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'actividad4/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
